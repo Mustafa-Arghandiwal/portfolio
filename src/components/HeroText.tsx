fcc6a3df00661666edc15fd7afa3d2d2edfe940c
Mustafa-Arghandiwal <mustafaarghandiwal2000@gmail.com>
2025-12-14 15:25:59 +0430

    Add responsiveness, including the Spline model

--- src/components/HeroText.tsx
+++ src/components/HeroText.tsx
@@ -1,18 +1,12 @@
 import { TextEffect } from '../../components/motion-primitives/text-effect';
-export default function() {
+export default function HeroText() {
 	return (
 
 
-		<div className='text-white font-orbitron font-bold '>
-			<TextEffect
-				delay={0} speedReveal={0.4}
-				className='text-6xl' per='char' as='h2' preset='blur'>Hey &mdash; I am Mustafa</TextEffect>
-			<TextEffect
-				delay={0} speedReveal={0.6}
-				className='text-5xl mt-8' per='char' as='h3' preset='blur'>I Build Websites,</TextEffect>
-			<TextEffect
-				delay={0} speedReveal={0.6}
-				className='text-5xl' per='char' as='h3' preset='blur'>and share what I learn along the way</TextEffect>
+		<div className='text-white  font-orbitron font-bold '>
+			<TextEffect delay={1} speedReveal={0.6} className='text-2xl big-tablet:text-6xl' per='char' as='h2' preset='blur'>Hey &mdash; I am Mustafa</TextEffect>
+			<TextEffect delay={2.5} speedReveal={0.6} className='text-lg xs:text-xl big-tablet:text-5xl mt-2 big-tablet:mt-8' per='char' as='h3' preset='blur'>I Build Websites,</TextEffect>
+			<TextEffect delay={4} speedReveal={0.6} className='text-lg xs:text-xl big-tablet:text-5xl' per='char' as='h3' preset='blur'>and share what I learn along the way</TextEffect>
 		</div>
 
 	)
