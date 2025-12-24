72ec42803b165b80eabc4b5d3827cbf0cd2105be
Mustafa-Arghandiwal <mustafaarghandiwal2000@gmail.com>
2025-12-24 14:13:40 +0430

    Add read time and date formatter utils

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,20 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-        // server: {
-        // 	allowedHosts: [
-        // 		"tclcq-43-230-209-210.a.free.pinggy.link"
-        // 	]
-        // }
-  },
-
-  integrations: [react()],
+	vite: {
+		plugins: [tailwindcss()],
+		server: {
+			allowedHosts: [
+				"cepdu-43-230-209-210.a.free.pinggy.link"
+			]
+		}
+	},
+	integrations: [react(), icon()],
+	devToolbar: {
+		enabled: false,
+	}
 });
