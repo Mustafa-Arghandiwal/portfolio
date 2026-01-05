ef937ab203f68febcbccb9f9f2412f2d9d130fd2
Mustafa-Arghandiwal <mustafaarghandiwal2000@gmail.com>
2026-01-05 10:37:22 +0430

    Change Homelayout to layout

--- src/components/HeroText.tsx
+++ src/components/HeroText.tsx
@@ -1,7 +1,7 @@
 import { TextEffect } from "../../components/motion-primitives/text-effect";
 export default function HeroText() {
 	return (
-		<div className="text-white  font-orbitron font-bold">
+		<div className="text-text  font-orbitron font-bold">
 			<TextEffect delay={0} speedReveal={0.6} className='text-2xl big-tablet:text-6xl' per='char' as='h2' preset='blur'>Hey &mdash; I am Mustafa</TextEffect>
 			<TextEffect delay={1.5} speedReveal={0.6} className='text-lg xs:text-xl big-tablet:text-5xl mt-2 big-tablet:mt-8' per='char' as='h3' preset='blur'>I Build Websites,</TextEffect>
 			<TextEffect delay={3} speedReveal={0.6} className='text-lg xs:text-xl big-tablet:text-5xl' per='char' as='h3' preset='blur'>and share what I learn along the way</TextEffect>
